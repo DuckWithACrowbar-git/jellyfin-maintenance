2f4bc9ed37a1b2716db28363ae395a363c7b9c7f
DuckWithACrowbar <8024382@student.harmonytx.org>
2026-09-04 12:19:43 -0500

    i have an idea :}

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
 import json
+import datetime import datetime
+
 
 app = Flask(__name__)
 
-scheduled_date = '{"from": "Sep. 5", "to": "Sep. 7"}'
+scheduled_date = '{"from": "Sep. 4 at 8:00 AM", "to": "Sep. 7 at 5:00 PM"}'
+countdown_info = '{"from": "", "to": ""}'
 date_data = json.loads(scheduled_date)
+
+# (Year, Month, Day, Hour, Minute, Second)
+start_epoch = datetime(2026, 9, 4, 8, 0, 0)
 
 
 @app.route("/", defaults={"path": ""})
